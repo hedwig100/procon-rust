6ad932181c632fa284cfdffe7bf936590621fb78
hedwig100 <pentagon7951@outlook.jp>
2022-03-27 16:59:45 +0900

    [add] io

---
+++ src/io.rs
@@ -0,0 +1,19 @@
+// This io package is useful when proconio cannot be used.
+pub fn input_intvec() -> Vec<usize> {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input
+        .trim()
+        .split(" ")
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect()
+}
+
+pub fn input_int2() -> (usize, usize) {
+    let v = input_intvec();
+    (v[0], v[1])
+}
+
+pub fn input_int() -> usize {
+    input_intvec()[0]
+}
